53186e31658d2b4d3f26161058d52e0317330906
farahm20 <mahboob.farah@gmail.com>
2020-11-24 14:22:09 +0100

    Refactoring the script file

--- readme.md
+++ readme.md
@@ -23,3 +23,11 @@
 18: check mediastream webApi mdn for media methods
 19: comment "async function allowCamera()" method
 20: In case or error Uncaught (in promise) DOMException: could not start video source.....when clicked "show camera video" we use try catch on start button method
+21: Add surge in terminal. npx surge
+after surge is installed, press enter
+22: copy the url and add https://thick-worm.surge.sh and run in browser
+23: npm i --save-dev surge
+24: add deploy in package json. Deploy scriot is required when one publish on the internet
+25: write this in termninal to deploy "npm run deploy"
+26: Add recording buttons in html and hidden download button
+27: 
--- script/script.js
+++ script/script.js
@@ -19,68 +19,75 @@ if ('serviceWorker' in navigator) {
 
 window.addEventListener('load', () => {
     if ('mediaDevices' in navigator) {
-        //selecting video elemnt and all its direct childrem (selecting all start stream that have video as a parent class)
-        const errorMessage = document.querySelector('.video > .error');
-        const startButton = document.querySelector('.video > .start-stream');
-        const stopButton = document.querySelector('.video > .stop-stream');
-        const photoButton = document.querySelector('.profile button');
-        const profilePic = document.querySelector('.profile > img');
-        let stream;
-
-        startButton.addEventListener('click', async() => {
-            errorMessage.innerHTML = ' ';
-          
-            try {
-                const md = navigator.mediaDevices;
-                stream = await md.getUserMedia({
-                    video: { width: 320, height: 320 }
-                })
-                const video = document.querySelector('.video > video');
-                video.srcObject = stream;
-                //after step 20
-                stopButton.disabled = false;
-                photoButton.disabled = false;
-                startButton.disabled = true;
-
-            } catch (e) {
-                errorMessage.innerHTML = 'Could not show camera window.';
-            } 
-            
-        })
-
-        stopButton.addEventListener('click', () => {
-            errorMessage.innerHTML = ' ';
-            if(stream) {
-                let tracks = stream.getTracks();
-                tracks.forEach(track => track.stop());
-                //after step 20
-                stopButton.disabled = true;
-                photoButton.disabled = true;
-                startButton.disabled = false;
-                errorMessage.innerHTML = 'Video stopped! ';
-
-                console.log('Stop button. Tracks: ', tracks);
-            } else {
-                errorMessage.innerHTML = 'No video to stop! ';
-            }
-        })
-
-        photoButton.addEventListener('click', async() => {
-            errorMessage.innerHTML = ' ';
-          
-            if(!stream) {
-                errorMessage.innerHTML = 'No video to take photo from';   
-                return;
-            }
-                
-                
-            let tracks = stream.getTracks();
-            let videoTrack = tracks[0];
-            let capture = new ImageCapture(videoTrack);
-            let blob = await capture.takePhoto();
-
-            let imgUrl = URL.createObjectURL(blob);
-            profilePic.src = imgUrl; 
-        })
+        cameraSettings();
     }
 })
+
+function cameraSettings() {
+    //selecting video elemnt and all its direct childrem (selecting all start stream that have video as a parent class)
+    const errorMessage = document.querySelector('.video > .error');
+    const startButton = document.querySelector('.video > .start-stream');
+    const stopButton = document.querySelector('.video > .stop-stream');
+    const photoButton = document.querySelector('.profile button');
+    const profilePic = document.querySelector('.profile > img');
+    const startRecording = document.querySelector('.video .start-recording');
+    const stopRecording = document.querySelector('.video .stop-recording');
+    const downloadLink = document.querySelector('.video .downloadLink');
+
+
+    let stream;
+
+    startButton.addEventListener('click', async () => {
+        errorMessage.innerHTML = ' ';
+
+        try {
+            const md = navigator.mediaDevices;
+            stream = await md.getUserMedia({
+                video: { width: 320, height: 320 }
+            })
+            const video = document.querySelector('.video > video');
+            video.srcObject = stream;
+            //after step 20
+            stopButton.disabled = false;
+            photoButton.disabled = false;
+            startButton.disabled = true;
+
+        } catch (e) {
+            errorMessage.innerHTML = 'Could not show camera window.';
+        }
+
+    })
+
+    stopButton.addEventListener('click', () => {
+        errorMessage.innerHTML = ' ';
+        if (stream) {
+            let tracks = stream.getTracks();
+            tracks.forEach(track => track.stop());
+            //after step 20
+            stopButton.disabled = true;
+            photoButton.disabled = true;
+            startButton.disabled = false;
+            console.log('Stop button. Tracks: ', tracks);
+        } else {
+            errorMessage.innerHTML = 'No video to stop! ';
+        }
+    })
+
+    photoButton.addEventListener('click', async () => {
+        errorMessage.innerHTML = ' ';
+
+        if (!stream) {
+            errorMessage.innerHTML = 'No video to take photo from';
+            return;
+        }
+
+
+        let tracks = stream.getTracks();
+        let videoTrack = tracks[0];
+        let capture = new ImageCapture(videoTrack);
+        let blob = await capture.takePhoto();
+
+        let imgUrl = URL.createObjectURL(blob);
+        profilePic.src = imgUrl;
+    })
+}
